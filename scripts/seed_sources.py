c5d6cf36853e57c9234c31149485a135a3a393f4
jamiddd <jamiddeka1@gmail.com>
2026-08-05 13:14:43 +0530

    Fix select import in seed_sources.py

--- scripts/seed_sources.py
+++ scripts/seed_sources.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import sys
+from sqlalchemy.future import select
 
 # Ensure root of repo/backend is in sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
